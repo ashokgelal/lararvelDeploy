f1b0ec68bfd6c37142a0471673627b5725542b01
Adam Miedema <armgitaar@yahoo.com>
2020-04-10 10:42:28 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Cleaver
+                    Something
                 </div>
 
                 <div class="links">
