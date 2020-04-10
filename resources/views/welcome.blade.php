d76c6877cf86473494cfbd4b77878870e31262da
Adam Miedema <armgitaar@yahoo.com>
2020-04-10 10:44:36 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Something
+                    Something Else
                 </div>
 
                 <div class="links">
